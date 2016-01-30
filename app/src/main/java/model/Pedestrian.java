ad1bc9d4d3f7a7366d50f3acbd1e38db2befd0cc
Petar <petar.stosic993@gmail.com>
2016-01-30 16:19:58 +0100

    Android aplikacija za prepoznavanje pesaka. Koriscena biblioteka openCV. Dodato dosta funkcionalnosti ali treba jos malo pritegnuti algoritam i logiku.

---
+++ app/src/main/java/model/Pedestrian.java
@@ -0,0 +1,40 @@
+package model;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint2f;
+
+/**
+ * Created by Petar on 26-Jan-16.
+ */
+public class Pedestrian {
+    private MatOfPoint2f pointsOfInterests;
+
+    public Mat getImage() {
+        return image;
+    }
+
+    public void setImage(Mat image) {
+        this.image = image;
+    }
+
+    private Mat image;
+
+    public Pedestrian(){
+        pointsOfInterests = new MatOfPoint2f();
+    }
+    public Pedestrian(MatOfPoint2f param, Mat im){
+        pointsOfInterests = param;
+        image = im;
+    }
+
+    public MatOfPoint2f getPointsOfInterests() {
+        return pointsOfInterests;
+    }
+
+    public void setPointsOfInterests(MatOfPoint2f pointsOfInterests) {
+        this.pointsOfInterests = pointsOfInterests;
+    }
+    public void clearArray(){
+        pointsOfInterests.empty();
+    }
+}
